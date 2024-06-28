9ecf4d514e9e85c7af29e60509cfd804669a6b5b
Audun Nes <aunes@dfds.com>
2024-06-28 14:00:17 +0200

    Replace Terraform with OpenTofu. Update other tools to latest version

---
+++ src/scripts/install-tofu.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+ZIPFILE=tofu_${OPENTOFU_VERSION}_linux_${BUILD_ARCHITECTURE_ARCH}.zip
+SHA256SUM_FILE=tofu_${OPENTOFU_VERSION}_SHA256SUMS
+
+CHECKSUM=$(sha256sum "${ZIPFILE}" | cut -f 1 -d ' ')
+EXPECTED_CHECKSUM=$(grep "${ZIPFILE}" tofu_*_SHA256SUMS | cut -f 1 -d ' ')
+
+if [[ "${CHECKSUM}" = "${EXPECTED_CHECKSUM}" ]]; then
+    unzip tofu_${OPENTOFU_VERSION}_linux_${BUILD_ARCHITECTURE_ARCH}.zip
+    mv tofu /usr/local/bin
+    chmod +x /usr/local/bin/tofu
+    tofu -install-autocomplete
+    ln -s /usr/local/bin/tofu /usr/local/bin/terraform
+else
+    exit 1
+fi
